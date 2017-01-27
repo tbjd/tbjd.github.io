269062595f2f4b8d332b48ab0c3e40f65e9ece86
Brodeur Tristan <tristanbrodeur@hotmail.com>
2017-01-27 15:28:35 -0500

    redesigne

--- js/index.js
+++ js/index.js
@@ -26,5 +26,15 @@ $(document).ready(function () {
         }
     });
 
+        var movementStrength = 25;
+        var height = movementStrength / $(window).height();
+        var width = movementStrength / $(window).width();
+        $("#myBackGround").mousemove(function(e){
+            var pageX = e.pageX - ($(window).width() / 2);
+            var pageY = e.pageY - ($(window).height() / 2);
+            var newvalueX = width * pageX * -1 - 25;
+            var newvalueY = height * pageY * -1 - 50;
+            $('#myBackGround').css("background-position", newvalueX+"px     "+newvalueY+"px");
+        });
 
 });
